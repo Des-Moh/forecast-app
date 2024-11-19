24bd7773faa0cb0a11b9a67571871ddf749df639
Des-Moh <desireemohlala@gmail.com>
2024-11-19 12:15:44 +0200

    search function using API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = `${temperature}°C`;
+}
+
+function searchCity(city) {
+  let apiKey = "32b370fab474tf940588ea443ao8cf66";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
