792f5292de6c9fdf0ec7c81eedc8f904f1812357
Des-Moh <desireemohlala@gmail.com>
2024-11-20 11:46:32 +0200

    Added more weather data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function updateWeather(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let temperature = Math.round(response.data.temperature.current);
   temperatureElement.innerHTML = `${temperature}°C`;
@@ -7,10 +8,28 @@ function updateWeather(response) {
   descriptionElement.innerHTML = response.data.condition.description;
   let humidityElement = document.querySelector("#humidity");
   let humidity = response.data.humidity;
-  humidityElement.innerHTML = `${humidity}%`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   let windElement = document.querySelector("#wind");
   let windSpeed = Math.round(response.data.wind.speed);
   windElement.innerHTML = `${windSpeed}km/h`;
+
+  let date = new Date(response.data.time * 1000);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = `Last updated on ${day} at ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
